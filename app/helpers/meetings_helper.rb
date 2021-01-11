a6d318759a23b6c255f14e91511c1eb625633196
MarilenaRoque <marilenas@id.uff.br>
2021-01-11 13:15:12 -0300

    Set availables hash
    - Building help method to return a hash of availability
    - Avalilables inside the params for new request

--- app/helpers/meetings_helper.rb
+++ app/helpers/meetings_helper.rb
@@ -1,5 +1,20 @@
 module MeetingsHelper
-    def availability(meetings, index)
-        
+    def availability(meetings)
+        available = {
+            8 => true,
+            9 => true,
+            10 => true,
+            11 => true,
+            12 => true,
+            13 => true,
+            14 => true,
+            15 => true,
+            16 => true,
+            17 => true
+        }
+        meetings.each do |meet|
+            available[meet.start_time.hour] = false
+        end
+        return available
     end
 end
