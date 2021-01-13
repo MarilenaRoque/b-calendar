6feee4eb3450b4001f262f80e79774c3b34562e8
MarilenaRoque <marilenas@id.uff.br>
2021-01-13 14:28:21 -0300

    Test user associations and validations

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+
+    describe 'validations' do
+        it { should validate_presence_of(:username) }
+        it { should validate_presence_of(:email) }
+        it { should validate_uniqueness_of(:username) }
+    end
+
+    describe 'associations' do
+        it { should have_many(:meetings) }
+    end
+end
